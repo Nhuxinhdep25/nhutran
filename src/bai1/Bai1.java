8d3022478d03c2bee657c139b0512769d98298e9
nhuch <nhuch@NhuTT>
2024-08-24 20:44:58 +0700

    Bai Tap

---
+++ src/bai1/Bai1.java
@@ -0,0 +1,17 @@
+package bai1;
+
+import java.util.Scanner;
+public class Bai1 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        //System.out.println("Enter an int number: ");
+        //int number = scanner.nextInt();
+        //System.out.println("number");
+        System.out.println("Nhap du lieu: ");
+        String name = scanner.next();
+        //String name = scanner.nextLine();
+        System.out.println("name: " + name);
+    }
+    
+}
